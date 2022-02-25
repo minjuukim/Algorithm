8b1f43fcd1f9696d0de8639a9213e944655bbd11
minjuukim <96512579+minjuukim@users.noreply.github.com>
2022-02-26 00:24:41 +0900

    0225 IM대비 / (두번째로 큰 정수 출력)

---
+++ BAEKJOON/BOJ_B3_10817_세수.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_B3_10817_세수 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[3];
+		for(int i=0; i<3; i++) {
+			arr[i] = sc.nextInt();
+		}
+				
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
+	}
+
+}
