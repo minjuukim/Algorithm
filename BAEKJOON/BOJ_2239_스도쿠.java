d2448b5e9d4cb833a488bb2372c3edf054bb64ce
minjuukim <96512579+minjuukim@users.noreply.github.com>
2022-04-07 00:37:39 +0900

    0406 실습 / 백트래킹

---
+++ BAEKJOON/BOJ_2239_스도쿠.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ_2239_스도쿠 {
+	static int[][] board;
+	static ArrayList<int[]> list;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		board = new int[9][9];
+		list = new ArrayList<>();
+		
+		for (int i = 0; i < 9; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < 9; j++) {
+				board[i][j] = str.charAt(j) - 48;
+				if(board[i][j] == 0) {
+					list.add(new int[] {i, j});
+				}
+			}
+		}
+		
+		dfs(0);
+		
+	}
+	
+	public static void dfs(int idx) {
+		
+		if(idx == list.size()) {
+			print();
+			System.exit(0);
+		}
+		
+		int r = list.get(idx)[0];
+		int c = list.get(idx)[1];
+		
+		boolean[] check = new boolean[10];
+		
+		// 행 체크
+		for (int i = 0; i < 9; i++) {
+			if(board[r][i] != 0) {
+				check[board[r][i]] = true;
+			}
+		}
+		
+		// 열 체크
+		for (int i = 0; i < 9; i++) {
+			if(board[i][c] != 0) {
+				check[board[i][c]] = true;
+			}
+		}
+		
+		// 3*3 사각형 체크
+		int startR = (r/3) * 3;
+		int startC = (c/3) * 3;
+		for (int i = startR; i < startR + 3; i++) {
+			for (int j = startC; j < startC + 3; j++) {
+				if(board[i][j] != 0) {
+					check[board[i][j]] = true;
+				}
+			}
+		}
+		
+		for (int i = 1; i < 10; i++) {
+			if(!check[i]) {
+				board[r][c] = i;
+				dfs(idx+1);
+				board[r][c] = 0;
+			}
+		}
+		
+	
+	}
+	
+	public static void print() {
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				sb.append(board[i][j]);
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
+
+}
