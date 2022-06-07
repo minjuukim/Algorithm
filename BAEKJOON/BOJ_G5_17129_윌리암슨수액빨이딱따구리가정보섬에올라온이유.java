bae71bf74b865f0f7d2d03e26bfb351ffbd95011
minjuukim <96512579+minjuukim@users.noreply.github.com>
2022-06-07 17:27:57 +0900

    0606 / BFS

--- BAEKJOON/BOJ_G5_17129_윌리암슨수액빨이딱따구리가정보섬에올라온이유.java
+++ BAEKJOON/BOJ_G5_17129_윌리암슨수액빨이딱따구리가정보섬에올라온이유.java
@@ -9,22 +9,26 @@ import java.util.StringTokenizer;
 
 public class BOJ_G5_17129_윌리암슨수액빨이딱따구리가정보섬에올라온이유 {
 	
+	static int N, M;
+	static int ans = Integer.MAX_VALUE;
+	static int[][] map;
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		int[][] map = new int[N][M];
+		map = new int[N][M];
 		int x=0, y=0;
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(i) - '0';
+				map[i][j] = str.charAt(j) - '0';
 				if(map[i][j] == 2) {
 					x = i;
 					y = j;
@@ -33,20 +37,51 @@ public class BOJ_G5_17129_윌리암슨수액빨이딱따구리가정보섬에올
 		}
 		
 		bfs(x, y);
+		if(ans==Integer.MAX_VALUE) {
+			sb.append("NIE");
+		} else {
+			sb.append("TAK").append("\n").append(ans);
+		}
+		System.out.println(sb);
 	}
 	
 	public static void bfs(int x, int y) {
-		Queue<int[]> que = new LinkedList<int[]>();
-		que.offer(new int[] {x, y});
+		Queue<Pos> que = new LinkedList<>();
+		que.offer(new Pos(x, y, 0));
+		
+		boolean[][] visited = new boolean[N][M];
+		visited[x][y] = true;
 		
 		while(!que.isEmpty()) {
-			int[] cur = que.poll();
-			int nx = cur[0];
-			int ny = cur[1];
-			
-			for
+			Pos cur = que.poll();
+			for (int d = 0; d < 4; d++) {
+				int nx = cur.x + dx[d];
+				int ny = cur.y + dy[d];
+				
+				if(nx<0 || nx>=N || ny<0 || ny>=M || map[nx][ny]==1 || visited[nx][ny]) continue;
+				
+				if(map[nx][ny] == 0) {
+					visited[nx][ny] = true;
+					que.offer(new Pos(nx, ny, cur.dist+1));
+				} else {
+					visited[nx][ny] = true;
+					ans = Math.min(ans, cur.dist+1);
+					return;
+				}
+			}
 		}
-		
 	}
-
+	
+	public static class Pos {
+		int x;
+		int y;
+		int dist;
+		
+		public Pos(int x, int y, int dist) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+		}
+	}
 }
