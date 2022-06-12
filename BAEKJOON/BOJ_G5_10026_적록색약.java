77cea7072a90013e16b7d144a1c6817f5a088367
minjuukim <96512579+minjuukim@users.noreply.github.com>
2022-06-13 01:27:58 +0900

    0612 / DFS

--- BAEKJOON/BOJ_G5_10026_적록색약.java
+++ BAEKJOON/BOJ_G5_10026_적록색약.java
@@ -6,15 +6,19 @@ import java.io.InputStreamReader;
 
 public class BOJ_G5_10026_적록색약 {
 	
+	static int N;
+	static char[][] board;
+	static boolean[][][] visited;
 	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		
-		char[][] board = new char[N][N];
+		board = new char[N][N];
+		visited = new boolean[N][N][2];
 		
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
@@ -23,11 +27,42 @@ public class BOJ_G5_10026_적록색약 {
 			}
 		}
 		
-		dfs();
+		int normal_cnt = 0;
+		int rgcb_cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if(!visited[i][j][0]) {		// 적록색약이 아닌 경우 방문체크
+					dfs(0, i, j);
+					normal_cnt++;
+				} 
+				if(!visited[i][j][1]) {		// 적록색약인 경우 방문체크
+					dfs(1, i, j);
+					rgcb_cnt++;
+				}
+			}
+		}
+		
+		System.out.println(normal_cnt+" "+rgcb_cnt);
 	}
 	
-	public static void dfs() {
+	public static void dfs(int rgcb, int r, int c) {	// 정상:rgcb=0, 적록색약:rgcb=1
 		
+		visited[r][c][rgcb] = true;
+		
+		for (int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if(nr<0 || nr>=N || nc<0 || nc>=N || visited[nr][nc][rgcb]) continue;
+			
+			if(rgcb==1 && (board[r][c]=='R' || board[r][c]=='G')) {
+				if(board[nr][nc]=='R' || board[nr][nc]=='G') {
+					dfs(1, nr, nc);
+				}
+			}
+			else if(board[r][c] == board[nr][nc]) {
+				dfs(rgcb, nr, nc);
+			}
+		}
 	}
-
 }
