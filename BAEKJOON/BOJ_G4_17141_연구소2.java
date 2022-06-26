f535eb870abca8b194f0f99d26ec50dbf40e9efd
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-06-26 23:58:30 +0900

    0626 / BFS, 조합

---
+++ BAEKJOON/BOJ_G4_17141_연구소2.java
@@ -0,0 +1,94 @@
+package day0626;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_G4_17141_연구소2 {
+	
+	static int N, M, map[][];
+	static ArrayList<Pos> vList = new ArrayList<>();	// 바이러스를 놓을 수 있는 칸 배열
+	static Pos[] virus;
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());	// 바이러스 개수
+		map = new int[N][N];
+		virus = new Pos[M];		// 바이러스가 있는 칸
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());	// 0:빈칸, 1:벽, 2:바이러스 놓을수 있는 칸
+				if(map[i][j]==2) {
+					vList.add(new Pos(i, j));
+				}
+			}
+		}
+		
+		comb(0, 0);
+	}
+	
+	// M개 바이러스 놓기
+	// 바이러스를 놓을수 있는 칸 중에서 바이러스 M개를 놓을 자리 선택
+	public static void comb(int start, int cnt) {
+		
+		if(cnt==M) {
+			bfs(virus);
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			virus[cnt] = vList.get(i);
+			comb(i+1, cnt+1);
+		}
+	}
+	
+	// 모든 빈칸에 바이러스 퍼뜨리는 최소시간 구하기
+	public static void bfs(Pos[] virus) {
+		Queue<Pos> que = new LinkedList<>();
+		int[][] times = new int[N][N];	// 바이러스가 퍼진 시간 저장한 배열
+		Arrays.fill(times, -1); 	// -1로 초기화
+		
+		for (int i = 0; i < M; i++) {
+			que.offer(new Pos(virus[i].r, virus[i].c));
+			times[virus[i].r][virus[i].c] = 0;	// 바이러스가 있는 칸:0
+		}
+		
+		while(!que.isEmpty()) {
+			Pos cur = que.poll();
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = cur.r + dr[d];
+				int nc = cur.c + dc[d];
+				
+				// 범위를 벗어나거나, 벽이거나, 이미 방문한 곳이라면 패스
+				if(nr<0 || nr>=N || nc<0 || nc>=N || map[nr][nc]==1 || times[nr][nc]!=-1) continue;
+				
+				
+			}
+		}
+	}
+	
+	public static class Pos {
+		int r;
+		int c;
+		
+		public Pos(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+	}
+
+}
