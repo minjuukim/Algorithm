735f46be38a43888692d6e513b83c4d0ef320e0a
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-07-05 23:54:35 +0900

    Add files via upload

---
+++ BAEKJOON/BOJ_S3_1166_선물.java
@@ -0,0 +1,22 @@
+package day0705;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S3_1166_선물 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());	// 작은박스 개수
+		int L = Integer.parseInt(st.nextToken());	// 큰 직육면체 크기
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		
+	}
+
+}
+
