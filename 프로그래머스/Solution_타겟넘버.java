da8917216d209a6b03a4aa641785ad389f619584
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-07-23 01:53:25 +0900

    0722 / BFS

--- 프로그래머스/Solution_타겟넘버.java
+++ 프로그래머스/Solution_타겟넘버.java
@@ -1,10 +1,40 @@
-package day0722;
+import java.io.*;
+import java.util.*;
 
-public class Solution_타겟넘버 {
+class Solution_타겟넘버 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static int ans;
+    static int[] symbol = {+1, -1};
 
-	}
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
 
+        bfs(numbers, target);
+        answer = ans;
+
+        return answer;
+    }
+
+    public static void bfs(int[] numbers, int target){
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{0, 0});
+        int N = numbers.length;
+
+        while(!que.isEmpty()){
+            int[] cur = que.poll();
+            int sum = cur[0];
+            int idx = cur[1];
+
+            if(idx==N){
+                if(sum==target) ans++;
+                continue;
+            }
+
+            int nxt = sum + symbol[0] * numbers[idx];
+            que.offer(new int[]{nxt, idx+1});
+
+            nxt = sum + symbol[1] * numbers[idx];
+            que.offer(new int[]{nxt, idx+1});
+        }
+    }
 }
