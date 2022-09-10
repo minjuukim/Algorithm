42b9dcff89580a9f01eb1cac65d34e7fd6be1f1d
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-09-11 00:17:52 +0900

    0910 / 투포인터

--- BAEKJOON/BOJ_G4_1253_좋다.java
+++ BAEKJOON/BOJ_G4_1253_좋다.java
@@ -1,16 +1,18 @@
 package day0909;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_G4_1253_ÁÁ´Ù {
+public class BOJ_G4_1253_ì¢‹ë‹¤ {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine());	// ¼öÀÇ °³¼ö
+		int N = Integer.parseInt(br.readLine());	// ìˆ˜ì˜ ê°œìˆ˜
 		int[] arr = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
@@ -18,6 +20,39 @@ public class BOJ_G4_1253_ÁÁ´Ù {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		Arrays.sort(arr);
+		
+		int count = 0;
+		
+		for (int k = 0; k < N; k++) {
+			int i = 0;
+			int j = N-1;
+			long key = arr[k];
+			
+			// íˆ¬í¬ì¸í„° ì•Œê³ ë¦¬ì¦˜
+			while(i<j) {
+				
+				int sum = arr[i] + arr[j];
+				
+				if(sum == key) {	// keyê°€ ì„œë¡œ ë‹¤ë¥¸ ë‘ ìˆ˜ì˜ í•©ì´ì–´ì•¼ í•¨ì„ ì²´í¬.
+					if(i!=k && j!=k) {
+						count++;
+						break;
+					} else if(i == k) {
+						i++;
+					} else if(j == k){
+						j--;
+					}
+				}
+				else if(sum < key) {
+					i++;
+				} else {
+					j--;
+				}
+			}
+		}
+		
+		System.out.println(count);
+		br.close();
 	}
-
 }
