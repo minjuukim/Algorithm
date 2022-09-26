7772399a94bd2d0ffdf587faf6747a47ea078dc4
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-09-26 23:58:54 +0900

    Add files via upload

--- BAEKJOON/BOJ_S2_2644_촌수계산.java
+++ BAEKJOON/BOJ_S2_2644_촌수계산.java
@@ -1,10 +1,21 @@
 package day0925;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_S2_2644_촌수계산 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());	// 전체 사람 수
+		
+		st = new StringTokenizer(br.readLine());
+		int p1 = Integer.parseInt(st.nextToken());
+		int p2 = Integer.parseInt(st.nextToken());
 	}
 
 }
