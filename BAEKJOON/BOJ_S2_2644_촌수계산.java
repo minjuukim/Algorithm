d4e3a18c0aa5191d00269288f3660dd56270ad15
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-09-27 00:23:24 +0900

    0927 / DFS, 그래프, 인접행렬

--- BAEKJOON/BOJ_S2_2644_촌수계산.java
+++ BAEKJOON/BOJ_S2_2644_촌수계산.java
@@ -6,16 +6,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_S2_2644_촌수계산 {
+	static int n, p2, count, ans=-1;
+	static int[][] map;
+	static boolean[] visited ;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int n = Integer.parseInt(br.readLine());	// 전체 사람 수
+		n = Integer.parseInt(br.readLine());	// 전체 사람 수
+		map = new int[n+1][n+1];
+		visited = new boolean[n+1];
 		
 		st = new StringTokenizer(br.readLine());
 		int p1 = Integer.parseInt(st.nextToken());
-		int p2 = Integer.parseInt(st.nextToken());
+		p2 = Integer.parseInt(st.nextToken());
+		
+		int m = Integer.parseInt(br.readLine());	// 관계의 개수
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			map[x][y] = 1;
+			map[y][x] = 1;
+		}
+		
+		visited[p1] = true;
+		dfs(p1);
+		
+		System.out.println(ans);
 	}
-
+	
+	public static void dfs(int i) {
+		
+		if(i==p2) {
+			ans = count;
+			return;
+		}
+		
+		for (int j = 1; j <= n; j++) {
+			if(map[i][j]==1 && !visited[j]) {
+				visited[j] = true;
+				count++;
+				dfs(j);
+				count--;
+			}
+		}
+	}
 }
