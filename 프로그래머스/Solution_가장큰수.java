eb3e827ecf48c999818b71c8b6cc624917ce0e0f
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-10-09 23:58:32 +0900

    Add files via upload

---
+++ 프로그래머스/Solution_가장큰수.java
@@ -0,0 +1,42 @@
+package day1009;
+
+public class Solution_가장큰수 {
+	
+	static int N;
+    static int max = 0;
+    static int[] isSelected, arr;
+
+	public static void main(String[] args) {
+
+		String answer = "";
+        N = numbers.length;
+        isSelected = new int[N];
+        arr = new int[N];
+        
+        return answer;
+	}
+	
+	// 순열
+    public void perm(int[] numbers, int cnt){
+        if(cnt==N) {
+            //Arrays.toString(arr).replaceAll("[^0-9]", "");
+            int num = 0;
+            for(int digit : arr) {
+                num *= 10;
+                num += digit;
+            }
+            max = 
+            return;
+        }
+            
+        for(int i=0; i<N; i++){
+            if(isSelected[i]) continue;
+            
+            arr[cnt] = numbers[i];
+            isSelected[i] = true;
+            perm(cnt+1);
+            isSelected[i] = false;
+        }
+    }
+
+}
