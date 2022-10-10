66f22163f55394d7906ab110ef733550fa90eb15
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-10-11 00:17:48 +0900

    1010 / 정렬, 문자열 비교, 람다식

--- 프로그래머스/Solution_가장큰수.java
+++ 프로그래머스/Solution_가장큰수.java
@@ -1,43 +1,40 @@
-package day1009;
+import java.util.*;
+import java.io.*;
 
-public class Solution_°¡ÀåÅ«¼ö {
-	
-	static int N;
-    static int max = 0;
-    static int[] isSelected, arr;
+class Solution {
 
-	public static void main(String[] args) {
-
-		String answer = "";
-        N = numbers.length;
-	String[] str = 
-        //isSelected = new int[N];
-        //arr = new int[N];
+    public String solution(int[] numbers) {
+        String answer = "";
+        int N = numbers.length;
+        String[]  str = new String[N];
         
-        return answer;
-	}
-	
-	// ¼ø¿­
-    public void perm(int[] numbers, int cnt){
-        if(cnt==N) {
-            //Arrays.toString(arr).replaceAll("[^0-9]", "");
-            int num = 0;
-            for(int digit : arr) {
-                num *= 10;
-                num += digit;
-            }
-            max = 
-            return;
-        }
-            
+        // int배열 -> String배열
         for(int i=0; i<N; i++){
-            if(isSelected[i]) continue;
-            
-            arr[cnt] = numbers[i];
-            isSelected[i] = true;
-            perm(cnt+1);
-            isSelected[i] = false;
+            str[i] = String.valueOf(numbers[i]);
         }
+        
+        // 내림차순 정렬
+        Arrays.sort(str, new Comparator<String>(){
+            @Override
+            public int compare(String o1, String o2){
+                return (o2+o1).compareTo(o1+o2);
+                // 오름차순 정렬: (o1+o2).compareTo(o1+o2);
+            }
+        });
+        
+        // 람다식 사용 버전
+        // Arrays.sort(str, (a,b)->{
+        //         return (a+b).compareTo(b+a);
+        //     }
+        // );
+        
+        // 입력 데이터가 모두 0인 경우 0으로 리턴 ex){0,0,0} -> 0
+        // 답이 000이 나오면 안되므로 첫번째 값이 0이면 0을 리턴.
+        if(str[0].equals("0")) return "0";
+        
+        // 0이 아니면 정렬한 모든 문자열을 더함.
+        for(String s : str) answer += s;
+ 
+        return answer;
     }
-
 }
