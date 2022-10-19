0d31c17f921a03e367887c865cf96c2cb7518b0d
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2022-10-19 23:58:52 +0900

    Add files via upload

--- BAEKJOON/BOJ_G5_1068_트리.java
+++ BAEKJOON/BOJ_G5_1068_트리.java
@@ -1,10 +1,14 @@
 package day1018;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BOJ_G5_1068_트리 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());	// 노드의 개수
 	}
 
 }
