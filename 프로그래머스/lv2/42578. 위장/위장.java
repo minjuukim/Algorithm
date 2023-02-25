05cc4580d2066e4a9bf9dcedf641ea7aaf7560c0
Minju Kim <96512579+minjuukim@users.noreply.github.com>
2023-02-25 22:49:46 +0900

    [level 2] Title: 위장, Time: 0.07 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+            if(map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            } else {
+                map.put(clothes[i][1], 1);    
+            }
+        }
+        
+        for(Integer c : map.values()){
+            answer *= c + 1;
+        }
+        
+        return answer-1;
+    }
+}
